0a706815944935ab3bb194d9f962d62b6a5780b9
e-wind <>
2012-05-16 20:01:55 +0300

    site v 1.0

--- footer.php
+++ footer.php
@@ -3,7 +3,12 @@
 		<table>
 			<tr>
                                 <td>Copyright &COPY; 2012 Ayrapetyan Tigran aka e-wind</td>
-				<td> </td>
+				<td><a href="https://github.com/e-wind/e-wind-site">Исходный код сайта</a></td>
+                                <td>
+                                <!-- START OF HIT COUNTER CODE -->
+                                    <script language="JavaScript" src="http://www.counter160.com/js.js?img=11"></script><br /><a href="http://www.000webhost.com"><img src="http://www.counter160.com/images/11/left.png" alt="Free web hosting" border="0" align="texttop"></a><a href="http://www.hosting24.com"><img alt="Web hosting" src="http://www.counter160.com/images/11/right.png" border="0" align="texttop"></a>
+				<!-- END OF HIT COUNTER CODE -->
+                                </td>
 			</tr>
 		</table>
 	</div> <!-- End "footer" -->
